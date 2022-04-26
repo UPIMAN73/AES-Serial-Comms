578500c1dc0632dc85758d8b99471812b59a3295
UPIMAN73 <joshcalzadi@gmail.com>
2022-04-26 18:49:40 -0400

    Changed AES Library to tiny_aes_c
    Updated code with a new library and connected ideas to support Serial.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
+/**
+ * @file main.cpp
+ * @author Joshua Calzadillas (upiman73@github.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-26
+ * 
+ * 
+ */
 #include <Arduino.h>
-// #include 
-#include "dumpmem.hpp"
-#include "AES/aes.hpp"
+#include "AES/aesmanagement.hpp"
 
 #define BUAD_RATE       9600
-#define MESSAGE_TEXT    "ARDUINO MKRWAN 1300"
-#define ROOT_KEY        "0123456789abcdef"
-#define IV_KEY          "0000000000000000"
 
 
+static void phex(uint8_t* str); // Print Hex
+
+
+/**
+ * @brief 
+ * Setup function runs the code once
+ * 
+ */
 void setup() 
 {
   // put your setup code here, to run once:
@@ -18,41 +30,26 @@ void setup()
     delay(10);
   }
 
-  Serial.println(MESSAGE_TEXT);
-  Serial.println(String(MESSAGE_TEXT).length());
-
-  // AES Definitions
-  uint8_t * plainText = convertString(MESSAGE_TEXT);
-  uint8_t * rkey = convertString(ROOT_KEY);
-  uint8_t * iv = convertString(IV_KEY);
-  // uint8_t * padded = pad(plainText, strlen(toStr(plainText)));
-  
-
-  // CBC Encryption
-  uint8_t * eout = encryptECB(plainText, strlen(toStr(plainText)), rkey);
-  // uint8_t * eout = encryptCBC(padded, strlen(toStr(padded)), rkey, iv);
-  Serial.print("\nEncrypted Text: \n");
-  Serial.println(strlen(toStr(eout)));
-  Serial.println(toStr(eout));
-
-  // CBC Decryption
-  uint8_t * dout = decryptECB(eout, strlen(toStr(eout)), rkey);
-  // uint8_t * dout = decryptCBC(eout, strlen(toStr(eout)), rkey, iv);
-  dout = removePad(dout, strlen(toStr(dout)));
-  String decryptedText = String(toStr(dout));
-  Serial.print("\n\nDecrypted Text: \n");
-  Serial.println(decryptedText.length());
-  Serial.println(decryptedText);
-
-  // dumpAll((void*) DESIRED_ADDRESS, (0x08FF - DESIRED_ADDRESS));
-  Serial.println(" ");
-  Serial.println(" ");
-  dumpAll(plainText, strlen(toStr(plainText)) + 8);
-  Serial.println(" ");
-  dumpAll(&decryptedText, decryptedText.length() + 8);
-  // dumpAll(toStr(padded), strlen(toStr(padded)) + 8);
+  // AES Settings
+  AES_init_ctx_iv(&ctx, KEY, IV); // initilizing the CTX environment
 }
 
+/**
+ * @brief 
+ * Loops function loops the code
+ * 
+ */
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+// prints string as hex
+static void phex(uint8_t* str)
+{
+    uint8_t len = 16;
+
+    unsigned char i;
+    for (i = 0; i < len; ++i)
+        Serial.print(str[i], HEX);
+    Serial.print("\n");
 }
